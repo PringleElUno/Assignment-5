b34aa42ed5af1e37fd5d6c23bc4ab55babbf67c3
PringleElUno <pringlel@oregonstate.edu>
2024-10-30 11:11:53 -0700

    taxicab

--- taxicab.py
+++ taxicab.py
@@ -17,40 +17,40 @@ class Taxicab:
         :param x: X-coordinate of the taxicab
         :param y: Y-coordinate of the taxicab
         """
-        self.__x_coord = x
-        self.__y_coord = y
-        self.__odometer = 0
+        self._x_coord = x
+        self._y_coord = y
+        self._odometer = 0
 
     def get_x_coord(self):
         """
         Returns the x-coordinate of the taxicab
         """
-        return self.__x_coord
+        return self._x_coord
 
     def get_y_coord(self):
         """
         Returns the y-coordinate of the taxicab
         """
-        return self.__y_coord
+        return self._y_coord
 
     def get_odometer(self):
         """
         Returns the total distance traveled for the odometer
         """
-        return self.__odometer
+        return self._odometer
 
     def move_x(self, distance):
         """
         Moves the taxi cab horizontally (the x-axis) by a given distance and updates the odometer
         :param distance: distance to move through the x-axis
         """
-        self.__x_coord += distance
-        self.__odometer += abs(distance)
+        self._x_coord += distance
+        self._odometer += abs(distance)
 
     def move_y(self, distance):
         """
         Moves the taxi cab vertically (the y-axis) by a given distance and updates the odometer
         :param distance: distance to move through the y-axis
         """
-        self.__y_coord += distance
-        self.__odometer += abs(distance)
+        self._y_coord += distance
+        self._odometer += abs(distance)
